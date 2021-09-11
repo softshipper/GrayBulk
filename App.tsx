fbd6c54266cc9ed16a85f79185f5d2fe97abc8d0
oceanstack <erickoston@hispeed.ch>
2021-09-11 23:11:12 +0200

    first commit

---
+++ App.tsx
@@ -0,0 +1,32 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from "react";
+import {
+  StyleSheet,
+  View,
+} from "react-native";
+
+
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "red" }}>
+      <View style={{ flex: 1, backgroundColor: "blue" }} />
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
